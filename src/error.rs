aba6eacf774f6e3e5b608cc40cc9afb4d960507f
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-06-29 21:27:29 +0700

    Add display to error

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,18 @@
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub enum Error {
     TokioError(tokio::io::Error),
     ReqwestError(reqwest::Error),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match &self {
+            Self::TokioError(err) => write!(f, "Tokio Error: {}", err),
+            Self::ReqwestError(err) => write!(f, "Reqwest Error: {}", err),
+        }
+    }
 }
 
 impl From<tokio::io::Error> for Error {
