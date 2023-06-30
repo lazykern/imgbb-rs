81e227d1053d7f43f87b9478e7e791eacea9238f
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-06-30 09:45:43 +0700

    Use std::error instead of tokio

--- src/error.rs
+++ src/error.rs
@@ -2,22 +2,24 @@ use std::fmt::Display;
 
 #[derive(Debug)]
 pub enum Error {
-    TokioError(tokio::io::Error),
+    IOError(std::io::Error),
     ReqwestError(reqwest::Error),
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match &self {
-            Self::TokioError(err) => write!(f, "Tokio Error: {}", err),
+            Self::IOError(err) => write!(f, "IO Error: {}", err),
             Self::ReqwestError(err) => write!(f, "Reqwest Error: {}", err),
         }
     }
 }
 
-impl From<tokio::io::Error> for Error {
-    fn from(err: tokio::io::Error) -> Self {
-        Self::TokioError(err)
+impl std::error::Error for Error {}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Self::IOError(err)
     }
 }
 
